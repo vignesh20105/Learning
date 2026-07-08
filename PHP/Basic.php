45c14d3a342d2f17dd5b9090160a8dd3c6eb5f06
vigneshk.2105 <vigneshkumar20105@gmail.com>
2026-07-08 12:06:35 +0530

    Initial Commit

---
+++ PHP/Basic.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo 'Hello World!<br>';
+
+?> 
+<h1>PHP Version</h1>
+<?php
+echo phpversion();
+?>
+
+
+</body>
+</html>
---
+++ PHP/Variables.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$txt = "W3Schools.com";
+echo "I love $txt! <br>";
+?>
+    <?php
+    $a = 5;
+    $b = 10;
+    echo "The Addition for $a + $b is ";
+    echo $a + $b ;
+    ?>
+</body>
+</html>
