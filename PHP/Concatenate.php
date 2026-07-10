4f4a25dbbbe79a817545d7ce9c0392f5e5ad967a
vigneshk.2105 <vigneshkumar20105@gmail.com>
2026-07-10 14:45:39 +0530

    Initial Commit

---
+++ PHP/Concatenate.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<pre>
+    <h1>Concatenate String</h1>
+<?php
+$x = "Hello";
+$y = "World";
+$z = $x . $y;
+echo $z;
+?>
+</pre>
+</body>
+</html>
--- PHP/ModifyString.php
+++ PHP/ModifyString.php
@@ -18,6 +18,26 @@
     $x = "Hello World!";
 echo strtolower($x);
 ?>
+<h1>STRING REPLACE</H1>
+<?php
+$x = "Hello World!";
+echo str_replace("World", "Dolly", $x);
+?> 
+ <h1>String Reverse</h1>
+ <?php
+$x = "Hello World!";
+echo strrev($x);
+?> 
+<h1>Trim String</H1>
+
+$x = "    Hello World! ";
+echo trim($x);
+<h1>Explode</h1>
+<?php
+$x = "hello lovely world";
+$y = explode(" " , $x);
+print_r($y);
+?>
     </pre>
 </body>
 </html>
