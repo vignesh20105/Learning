eeaf2571f1c9fd819e449f6a090b00ed32c0ee97
vigneshk.2105 <vigneshkumar20105@gmail.com>
2026-07-13 11:33:00 +0530

    Initial commit

--- PHP/Casting.php
+++ PHP/Casting.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Casting</title>
 </head>
 <body>
-    
+    <pre>
+    <?php
+    $a = 5;
+
+    $a = (string) $a;
+
+    var_dump($a);
+    ?>
+    </pre>
 </body>
 </html>
