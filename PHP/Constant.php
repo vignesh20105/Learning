08643bb70b3c30b65fce8296b4dd289bafbc95b7
vigneshk.2105 <vigneshkumar20105@gmail.com>
2026-07-15 11:58:01 +0530

    Initial Commit

---
+++ PHP/Constant.php
@@ -0,0 +1,10 @@
+<?php
+
+const letter = "Hello World";
+//const letter = "Hello";
+
+define("CARS", array("Volvo", "BMW", "Toyota"));
+echo CARS[0];
+echo "<br>";
+echo letter;
+?>
---
+++ PHP/if.php
@@ -0,0 +1,9 @@
+<?php
+if(5>8)
+    {
+        echo "Correct";
+    }
+    else{
+        echo "Incorrect";
+    }
+    ?>
