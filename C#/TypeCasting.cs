992ea225c0ba857f73497af0b524604bdb992dd6
vigneshk.2105 <vigneshkumar20105@gmail.com>
2026-07-18 01:00:58 +0530

    Initial Commit

---
+++ C#/TypeCasting.cs
@@ -0,0 +1,4 @@
+float num = 16.345F;
+int num1 = (int)num;
+Console.WriteLine(num);
+Console.WriteLine(num1);
---
+++ C#/UserInput.cs
@@ -0,0 +1,3 @@
+Console.WriteLine("enter a name");
+string name = Console.ReadLine();
+Console.WriteLine(name);
