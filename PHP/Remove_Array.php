0c6d1696e5c01f7eac363e2906cdd22a8c1b1e57
vigneshk.2105 <vigneshkumar20105@gmail.com>
2026-07-21 13:04:16 +0530

    Initial Commit

--- PHP/Remove_Array.php
+++ PHP/Remove_Array.php
@@ -1,2 +1,18 @@
-<h2>Removing an array using Slice</h2>
+<h2>Removing an array using Splice</h2>
 <?php
+$cars = array("Volvo", "BMW", "Toyota");
+array_splice($cars,1,1);
+var_dump($cars);
+?>
+<H2>Removing Multiple Items from Array</h2>
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+array_splice($cars,1,2);
+var_dump($cars);
+?>
+<h2>Removing array value using index number using unset</h2>
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+unset($cars[0],$cars[1]);
+var_dump($cars);
+?>
